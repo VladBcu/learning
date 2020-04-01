3a7951dc46686606038193723a673ea632e32807
Vlad <vlad.bcu@gmail.com>
2020-04-01 17:08:46 +0100

    modified some comments inside

--- array1.php
+++ array1.php
@@ -1,7 +1,7 @@
 <?php
 
-$names = array ('Vlad' , 'Lavi' , 'Mini'); //variable assigned as array
-$ages = array ('Vlad'=>31, 'Lavi'=>29, 'Mini'=>6);
+$names = ['Vlad' , 'Lavi' , 'Mini']; //variable assigned as array
+$ages = ['Vlad'=>31, 'Lavi'=>29, 'Mini'=>6];
 
 /* Don't forget numbering is from 0
 Vlad = 0
@@ -9,11 +9,16 @@ Lavi = 1
 Mini = 2
 */
 
+require 'index.view.php';
+
 echo $names [0] . '<br>';
-echo $ages ['Vlad'].'<br>'; //you don't necessarily have to have space between all
+echo $ages ['Vlad'].'<br>'; //This will echo the corresponding value for the certain identifyier
 
-echo $names[0] . ' is ' . $ages ['Vlad'] . ' years old.'.'<br>';
 
-print_r ($ages); //used to display array
+echo $names[0].' is '.$ages['Vlad'].' years old.'.'<br>';
+
+print_r ($ages); //used to display the whole array
+
+
 
 ?>
