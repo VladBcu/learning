feffde78406ca9fa594b0c55eb0494cf9237bfd7
Vlad <vlad.bcu@gmail.com>
2020-04-01 17:09:24 +0100

    added a comment

--- writefile2.php
+++ writefile2.php
@@ -1,7 +1,7 @@
 <?php
 
-$file = "./lines.txt";
-$write = "This is line two\n";
+$file = "./lines.txt"; //create the file
+$write = "This is line two\n"; //text to be added
 
 file_put_contents ($file, $write, FILE_APPEND); //the append adds to the existing file text
 
